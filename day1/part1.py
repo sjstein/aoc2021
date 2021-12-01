9318ab7284fc531671b22eff20a765dbd7dc4101
Josh Stein <s.joshua.stein@gmail.com>
2021-12-01 05:55:58 -0600

    initital codebasae

--- day1/part1.py
+++ day1/part1.py
@@ -1,1 +1,16 @@
 # D1P1
+
+fname = 'input_p1'
+fp = open(fname, 'r')
+col = fp.read().split()
+
+inc = 0
+print(col)
+for num in range(1, len(col)):
+    if col[num] > col[num-1]:
+        inc += 1
+        print(f'comp# {num} : {col[num]} > {col[num - 1]}? YES (inc = {inc})')
+    else:
+        print(f'comp# {num} : {col[num]} > {col[num - 1]}? NO  (inc = {inc})')
+
+print(f'result={inc}')
