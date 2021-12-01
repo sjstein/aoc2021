1750e047f41e03e2526f59d715ecd6af5d89aa78
sjstein <s.joshua.stein@gmail.com>
2021-12-01 11:05:12 -0600

    cleaned up description. added debugging lines ftl

--- day1/part1.py
+++ day1/part1.py
@@ -2,15 +2,14 @@
 
 fname = 'input_p1'
 fp = open(fname, 'r')
-col = fp.read().split()
+depths = fp.read().split()
 
-inc = 0
-print(col)
-for num in range(1, len(col)):
-    if col[num] > col[num-1]:
+inc = 0     # Counter for each time a depth increases from one element to the next in the series
+for num in range(1, len(depths)):
+    if depths[num] > depths[num-1]:
         inc += 1
-        print(f'comp# {num} : {col[num]} > {col[num - 1]}? YES (inc = {inc})')
+        print(f'comp# {num} : {depths[num]} > {depths[num - 1]}? YES (inc = {inc})')
     else:
-        print(f'comp# {num} : {col[num]} > {col[num - 1]}? NO  (inc = {inc})')
+        print(f'comp# {num} : {depths[num]} > {depths[num - 1]}? NO  (inc = {inc})')
 
 print(f'result={inc}')
