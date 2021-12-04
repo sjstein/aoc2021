f7d893f6ce66e7871be2609a0a1cdbfd938cffa6
Josh Stein <s.joshua.stein@gmail.com>
2021-12-04 06:41:39 -0600

    Shell

---
+++ day4/part1.py
@@ -0,0 +1,6 @@
+# D4P1
+
+fname = 'input_p1'
+fp = open(fname, 'r')
+diag = fp.read().split('\n')
+
---
+++ day4/part2.py
@@ -0,0 +1,5 @@
+# D4P2
+
+fname = 'input_p1'
+fp = open(fname, 'r')
+diag = fp.read().split('\n')
